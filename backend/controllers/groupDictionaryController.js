fa361d1b99b1003e4d3dc14188ff05695a4cecb4
Rodrigo Rossiter <rossiter.gf@gmail.com>
2025-03-26 17:24:17 -0300

    adding groupDictionary route in server.js

--- backend/controllers/groupDictionaryController.js
+++ backend/controllers/groupDictionaryController.js
@@ -1,19 +1,18 @@
 const { GroupDictionary } = require("../models/DicionarioGrupo");
 
-const getGroupDictionary = async (req, res) => {
-    try {
-        const groupDictionary = (await GroupDictionary.find()).filter(dictionary => !dictionary.deletedAt);
-        if (groupDictionary) {
-            res.status(200).json(groupDictionary);
-        } else {
-            res.status(404).json({ message: "Grupo não encontrado" });
-        }
-      } catch (error) {
-        res.status(500).json({ message: error.message });
-      }
-};
+class GroupDictionaryController{
+    async getGroupDictionaries(req, res) {
+        try {
+            const groupDictionary = (await GroupDictionary.find()).filter(dictionary => !dictionary.deletedAt);
+            if (groupDictionary) {
+                res.status(200).json(groupDictionary);
+            } else {
+                res.status(404).json({ message: "Grupo não encontrado" });
+            }
+          } catch (error) {
+            res.status(500).json({ message: error.message });
+          }
+    };
+}
 
-module.exports = {
-    getGroupDictionary,
-};
-
+export default new GroupDictionaryController();
